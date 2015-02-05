3e4dced2d5e4dddd3f67ab50d82d6465a06b52c8
Tadej Panjtar <tadej@localhost.localdomain>
2015-02-05 10:46:38 +0100

    first commit

---
+++ 1strategy/Tadej_test.cpp
@@ -0,0 +1,81 @@
+/*
+ * Tadej_test.cpp
+ *
+ *  Created on: 30. jan. 2015
+ *      Author: Tadej
+ */
+#include <iostream>
+#include <vector>
+#include "Tadej.h"
+
+using namespace std;
+
+class base
+{
+
+public:
+    base(){cout<<"Base Constructor Called\n";}
+    virtual ~base(){cout<<"Base Destructor called\n";}
+
+};
+class derived1:public base
+{
+
+public:
+    derived1(){cout<<"Derived constructor called\n";}
+    ~derived1(){cout<<"Derived destructor called\n";}
+
+};
+
+void i( string& str) {
+	cout << "" << str.c_str() << endl;
+	string s1 = "baba";
+	string s2 = "dec";
+	if (s1.compare(s2) == 0)
+		cout << "enaka" << endl;
+	else if (s1.compare(s2) < 0)
+		cout << "manjsr" << endl;
+	else
+		cout << "veche" << endl;
+}
+
+template <typename T>
+T Maxi(T a, T b) { return a<b ? b : a; }
+
+int main()
+{
+	vector<int> vec;
+	vec.push_back(12);
+	vec.push_back(334);
+
+	  cout << "vec contains:";
+	  for (vector<int>::iterator it = vec.begin() ; it != vec.end(); ++it)
+	    std::cout << ' ' << *it;
+	  cout << endl;
+
+    base* b;
+    b=new derived1;
+    delete b;
+
+    string s;
+    s = "TP";
+
+    i(s);
+    int kapaciteta = s.capacity();
+//    s += " Panjtar";
+    cout << s.c_str() << " " << kapaciteta << endl << "---------------" << endl;
+
+    FlyBehavior *f;
+    f = new FlyRocketPowered;
+    FlyRocketPowered fr;
+//    f->fly();
+
+    Duck *md = new MallardDuck;
+    Duck *mo = new ModelDuck;
+    mo->performFly();
+    mo->performQuack();
+    mo->display();
+//    mo->setFlyBehavior(fr); // dela, a imamo raje z pointerji narejeno
+    mo->setFlyBehavior2(f);
+    mo->performFly();
+}
