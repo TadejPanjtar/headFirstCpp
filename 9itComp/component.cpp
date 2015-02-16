a392e2cda43c029054a8b509ff5ac0c9e0381466
Tadej Panjtar <tadej.panjtar@gmail.com>
2015-02-16 17:54:32 +0100

    components describe and compile

--- 9itComp/component.cpp
+++ 9itComp/component.cpp
@@ -1,6 +1,13 @@
 #include "Menu.h"
 #include "Waitress.h"
 
+/*
+ * THE COMPOSITE PATTERN allows you to
+ * compose objects into tree structures to
+ * represent part-whole hierarchies. Composite
+ * lets clients treat individual objects and
+ * compositions of objects uniformly.
+ */
 int main()
 {
     MenuComponent *pancakeHouseMenu = 
